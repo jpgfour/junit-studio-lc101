21e8dbd363505de8d65419a6c8d3defc51f50257
jpgfour <jeremy.grise@gmail.com>
2020-05-07 18:22:03 -0500

    implemented 2 given test plus 12 jpg tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -36,5 +36,49 @@ public class BalancedBracketsTest {
     public void lastBracketNotLeftBracket() {
         assertFalse(BalancedBrackets.hasBalancedBrackets("[]]["));
     }
+    // test the example strings
+    // balanced - "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+    //
+    // unbalanced - "[LaunchCode", "Launch]Code[", "[", "]["
 
+    //jpg test 5 - example 1
+    @Test
+    public void example1True() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+    //jpg test 6 - example 2
+    @Test
+    public void example2True() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+    //jpg test 7 - example 3
+    @Test
+    public void example3True() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+    //jpg test 8 - example 4
+    @Test
+    public void example4False() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+    //jpg test 9 - example 5
+    @Test
+    public void example5False() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+    //jpg test 10 - bad middle brackets = false []][[]
+    @Test
+    public void badMiddleBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]][[]"));
+    }
+    //jpg test 11 - nicely nested = true [[][]]
+    @Test
+    public void nicelyNested() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[][]]"));
+    }
+    //jpg test 12 - bad pattern = false [[]][]][
+    @Test
+    public void badPattern() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]][]]["));
+    }
 }
