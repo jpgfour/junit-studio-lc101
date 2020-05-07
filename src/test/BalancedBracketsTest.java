0596fb736d4bd3a8d938ed755d3e676c76f1fe90
jpgfour <jeremy.grise@gmail.com>
2020-05-07 01:38:52 -0500

    setup for studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,7 +1,7 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
@@ -10,6 +10,11 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+    //freebie test
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
 
